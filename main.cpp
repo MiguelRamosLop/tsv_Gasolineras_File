bd1ea4c7bb17a10dd9733d5d062d2ef705b3dc54
Miguel Ramos López <miguelon2001b@gmail.com>
2021-01-28 12:03:32 +0100

    ultimas funciones y leer desde inicio y final

--- main.cpp
+++ main.cpp
@@ -41,7 +41,8 @@ struct gasolinera {
 ofstream salida ("../gasolineras_data.txt");
 
 //Función para crear la lista con los datos del archivo
-void readFile(gasolinera *&lista, gasolinera *&final);
+void readFileDesdeInicio(gasolinera *&lista, gasolinera *&final); //LL
+void readFileDesdeFinal(gasolinera *&pila); //pila
 void searchMunicipiosProv(gasolinera *lista, string provincia);
 void printMunicipiosProv(gasolinera *lista, string provincia);
 void EGasofaProv(gasolinera *lista, string provincia, string municipio, string localidad);
@@ -54,9 +55,11 @@ void gasolinaRepsol(gasolinera *lista, string provincia, string municipio, strin
 void direccionDosPrimeras (gasolinera *lista, string localidad);
 void gasolinasProxCalleToledo (gasolinera *lista);
 void triangularGasolinerasCodigoPostal(gasolinera *lista, int codigopostal);
+void leer5primeras (gasolinera *lista);
+void leer5ultimas (gasolinera *lista);
 
 
-void readFile(gasolinera *&lista, gasolinera *&final){
+void readFileDesdeInicio(gasolinera *&lista, gasolinera *&final){
     //Indicamos al programa la dirección del archivo que tiene que leer
     ifstream read("../hoja_modificada_UK.tsv");
     try{
@@ -81,6 +84,46 @@ void readFile(gasolinera *&lista, gasolinera *&final){
                 final = actual;
                 //actual->next = lista;
                 //lista = actual;
+            }
+
+        }
+        int opt;
+        cout << "Mostrar por consola (1/) Mostrar en file (2/)";
+        cin >> opt;
+        if (opt == 1) {
+            cout << "El archivo se leyo con exito";
+        } if (opt == 2) {
+            salida << "El archivo se leyo con exito";
+        }
+        //Si el archivo no se puede leer, salta la excepción
+    }catch(...){
+        read.close();
+        cout<<"El archivo no se ha podido leer."<<endl;
+    }
+}
+
+void readFileDesdeFinal(gasolinera *&pila) {
+    //Indicamos al programa la dirección del archivo que tiene que leer
+    ifstream read("../hoja_modificada_UK.tsv");
+    try{
+        //Mientras el archivo no esté vacío
+        while(!read.eof()){
+            gasolinera *actual= new gasolinera();
+            //Creamos un nodo para indicar al programa los datos que se van a encontrar en el archivo y su orden respectivo
+            read >>actual->provincia>>actual->municipio>> actual->localidad>> actual->codigoPostal>> actual->direccion>> actual->margen>> actual->longitud>> actual->latitud>> actual->precio95E5>>
+                 actual->precio95E10>> actual->precio95E5Premium>> actual->precio98E5>> actual->precio98E10>> actual->precioGasoleoA>> actual->precioGasoleoPremium>>
+                 actual->precioGasoleoB>> actual->precioGasoleoC>> actual->precioBioetanol>> actual->bioalcohol>> actual->precioBiodiesel>> actual->estermetilico>>
+                 actual->precioGasesLicuados>> actual->precioGasNaturalComprimido>> actual->precioGasNaturalLicuado>> actual->precioHidrogeno>> actual->rotulo>>
+                 actual->tipoVenta>> actual->rem>> actual->horario>> actual->tipoServicio;
+            //Una vez leído el fichero añadimos el nodo a la lista
+            if (pila==NULL){
+                actual->next = NULL;
+                pila = actual;
+            }else{
+                //lista->next=actual;
+                //actual = lista;
+                actual->next = pila;
+                pila = actual;
             }
         }
         int opt;
@@ -260,7 +303,7 @@ void gasolinaPlace(gasolinera *lista, string provincia, string municipio, string
         cout << "los precios de las gasolineras de lo ingresado son: " << endl;
         do {
             if (guia->provincia == provincia && guia->municipio==municipio && guia->localidad==localidad) {
-                cout << guia->precio95E5;
+                cout << guia->precio95E5 << endl;
             }
             guia = guia -> next;
         } while (guia != NULL);
@@ -268,7 +311,7 @@ void gasolinaPlace(gasolinera *lista, string provincia, string municipio, string
         salida << "los precios de las gasolineras de lo ingresado son: " << endl;
         do {
             if (guia->provincia == provincia && guia->municipio==municipio && guia->localidad==localidad) {
-                salida << guia->precio95E5;
+                salida << guia->precio95E5 << endl ;
             }
             guia = guia -> next;
         } while (guia != NULL);
@@ -346,64 +389,79 @@ void triangularGasolinerasCodigoPostal(gasolinera *lista, int codigopostal) {
     } while (guide != NULL);
 }
 
+void leer5primeras (gasolinera *lista) {
+    int contadorPrim = 0;
+    gasolinera *guia = lista;
+    do {
+        cout << "Direccion :" << guia -> direccion << endl;
+        contadorPrim++;
+        guia = guia -> next;
+    } while (contadorPrim <= 5);
+}
+
+void leer5ultimas (gasolinera *pila) {
+    int contadorUlt = 0;
+    gasolinera *guia = pila;
+    do {
+        cout << "Direccion :" << guia -> direccion << endl;
+        contadorUlt++;
+        guia = guia -> next;
+    } while (contadorUlt < 5);
+}
+
 int main() {
     gasolinera *iList = NULL;
     gasolinera *iFinal = NULL;
+    gasolinera *iPila = NULL;
     string prov, mun, loc, gas;
     int input, inputS, codPos;
-    cout << "Leyendo archivo...\n";
-    salida << "Leyendo archivo...\n";
-    readFile (iList, iFinal);
-    cout << iList -> provincia;
     do {
         cout << endl;
         cout << "\n|-------------------------------------|";
         cout << "\n|           - GASOLINERAS -           |";
         cout << "\n|------------------|------------------|";
-        cout << "\n| 1. Num municipios en una provincia  |";
-        cout << "\n| 2. Municipios de una provincia      |";
-        cout << "\n| 3. Media gasolina en provincia      |";
-        cout << "\n| 4. Gasolina mas cara                |";
-        cout << "\n| 5. Gasolina mas barata              |";
-        cout << "\n| 6. Codigo postal menor              |";
-        cout << "\n| 7. Codigo postal mayor              |";
-        cout << "\n| 8. Gasolinas de un sitio            |";
-        cout << "\n| 9. Gasolineras repsol               |";
-        cout << "\n| 10. Direccion dos primeras          |";
-        cout << "\n| 11. Gasolinas prox calle Toledo 98  |";
-        cout << "\n| 12. Triangular gasolineras cerca cp |";
-        cout << "\n| 13. Salir                           |";
+        cout << "\n| 1. Crear lista enlazada (L->F)      |";
+        cout << "\n| 2. Crear pila (F->L)                |";
+        cout << "\n| 3. Num municipios en una provincia  |";
+        cout << "\n| 4. Municipios de una provincia      |";
+        cout << "\n| 5. Media gasolina en provincia      |";
+        cout << "\n| 6. Gasolina mas cara                |";
+        cout << "\n| 7. Gasolina mas barata              |";
+        cout << "\n| 8. Codigo postal menor              |";
+        cout << "\n| 9. Codigo postal mayor              |";
+        cout << "\n| 10. Gasolinas de un sitio           |";
+        cout << "\n| 11. Gasolineras repsol              |";
+        cout << "\n| 12. Direccion dos primeras          |";
+        cout << "\n| 13. Gasolinas prox calle Toledo 98  |";
+        cout << "\n| 14. Triangular gasolineras cerca cp |";
+        cout << "\n| 15. Leer las 5 primeras             |";
+        cout << "\n| 16. Leer las 5 ultimas              |";
+        cout << "\n| 17. Salir                           |";
         cout << "\n|------------------|------------------|";
         cout << "\n\n Escoja una Opcion: ";
         cin >> input;
         switch (input) {
             case 1:
+                cout << "Leyendo archivo...\n";
+                salida << "Leyendo archivo...\n";
+                readFileDesdeInicio(iList, iFinal);
+                cout << iList -> municipio;
+                break;
+            case 2:
+                cout << "Leyendo archivo...\n";
+                salida << "Leyendo archivo...\n";
+                readFileDesdeFinal(iPila);
+                cout << iPila -> municipio;
+                break;
+            case 3:
                 cout << "Ingrese provincia: " ;
                 cin >> prov;
                 searchMunicipiosProv(iList, prov);
                 break;
-            case 2:
-                cout << "Ingrese provincia: ";
-                cin >> prov;
-                printMunicipiosProv(iList, prov);
-                break;
-            case 3:
-                cout << "Ingrese provincia: ";
-                cin >> prov;
-                cout << "Ingrese municipio: ";
-                cin >> mun;
-                cout << "Ingrese localidad: ";
-                cin >> loc;
-                EGasofaProv(iList,prov,mun,loc);
-                break;
             case 4:
                 cout << "Ingrese provincia: ";
                 cin >> prov;
-                cout << "Ingrese municipio: ";
-                cin >> mun;
-                cout << "Ingrese localidad: ";
-                cin >> loc;
-                gasolinaMasCara(iList,prov,mun,loc);
+                printMunicipiosProv(iList, prov);
                 break;
             case 5:
                 cout << "Ingrese provincia: ";
@@ -412,7 +470,7 @@ int main() {
                 cin >> mun;
                 cout << "Ingrese localidad: ";
                 cin >> loc;
-                gasolinaMasCheap(iList,prov,mun,loc);
+                EGasofaProv(iList,prov,mun,loc);
                 break;
             case 6:
                 cout << "Ingrese provincia: ";
@@ -421,7 +479,7 @@ int main() {
                 cin >> mun;
                 cout << "Ingrese localidad: ";
                 cin >> loc;
-                gasolinaCPMenor(iList,prov,mun,loc);
+                gasolinaMasCara(iList,prov,mun,loc);
                 break;
             case 7:
                 cout << "Ingrese provincia: ";
@@ -430,7 +488,7 @@ int main() {
                 cin >> mun;
                 cout << "Ingrese localidad: ";
                 cin >> loc;
-                gasolinaCPMayor(iList,prov,mun,loc);
+                gasolinaMasCheap(iList,prov,mun,loc);
                 break;
             case 8:
                 cout << "Ingrese provincia: ";
@@ -439,7 +497,7 @@ int main() {
                 cin >> mun;
                 cout << "Ingrese localidad: ";
                 cin >> loc;
-                gasolinaPlace(iList,prov,mun,loc);
+                gasolinaCPMenor(iList,prov,mun,loc);
                 break;
             case 9:
                 cout << "Ingrese provincia: ";
@@ -448,23 +506,50 @@ int main() {
                 cin >> mun;
                 cout << "Ingrese localidad: ";
                 cin >> loc;
-                gasolinaRepsol(iList,prov,mun,loc);
+                gasolinaCPMayor(iList,prov,mun,loc);
                 break;
             case 10:
+                cout << "Ingrese provincia: ";
+                cin >> prov;
+                cout << "Ingrese municipio: ";
+                cin >> mun;
+                cout << "Ingrese localidad: ";
+                cin >> loc;
+                gasolinaPlace(iList,prov,mun,loc);
+                break;
+            case 11:
+                cout << "Ingrese provincia: ";
+                cin >> prov;
+                cout << "Ingrese municipio: ";
+                cin >> mun;
+                cout << "Ingrese localidad: ";
+                cin >> loc;
+                gasolinaRepsol(iList,prov,mun,loc);
+                break;
+            case 12:
                 cout << "Ingrese localidad: ";
                 cin >> loc;
                 direccionDosPrimeras(iList,loc);
                 break;
-            case 11:
+            case 13:
                 //40.40831082064327, -3.7110372156691955
                 gasolinasProxCalleToledo (iList);
                 break;
-            case 12:
+            case 14:
                 cout << "Ingrese codigo postal: ";
                 cin >> codPos;
                 triangularGasolinerasCodigoPostal(iList, codPos);
                 break;
-            case 13:
+            case 15:
+                cout << "Imprimiendo las 5 primeras:" << endl;
+                leer5primeras(iList);
+                break;
+            case 16:
+                readFileDesdeFinal(iPila);
+                cout << "Imprimiendo las 5 ultimas:" << endl;
+                leer5ultimas(iPila);
+                break;
+            case 17:
                 cout << "Programa finalizado.";
                 break;
             default:
